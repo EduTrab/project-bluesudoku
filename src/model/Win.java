0018d0135e04ccadb906ddd9e495fe92597abb38
Carson He <carsonhe@Carsons-Air.usi.ch>
2022-04-27 22:07:39 +0200

    Create class Win to test if the success conditions are met. (TODO: one last method)

--- src/model/Win.java
+++ src/model/Win.java
@@ -22,5 +22,46 @@ public class Win
         ifwin = false;
     }
 
+    /**
+     * Return true if there is no same elements in the row, otherwise return false
+     * @param  row The row needs to be tested
+     * @return True if there is no same elements in the row, otherwise return false
+
+     */
+    public boolean testRow(Row row)
+    {
+        HashSet<Cell> set = new HashSet<Cell>(row.getCells());
+        ifwin = row.getCells().size() == set.size();
+        return ifwin;
+    }
     
+    /**
+     * Return true if there is no same elements in the column, otherwise return false
+     * @param  row The row needs to be tested
+     * @return True if there is no same elements in the column, otherwise return false
+
+     */
+    public boolean testColumn(Column column)
+    {
+        HashSet<Cell> set = new HashSet<Cell>(column.getCells());
+        ifwin = column.getCells().size() == set.size();
+        return ifwin;
+    }
+    
+    /**
+     * Return true if there is no same elements in the subgrid, otherwise return false
+     * @param  row The row needs to be tested
+     * @return True if there is no same elements in the subgrid, otherwise return false
+
+     */
+    public boolean testColumn(SubGrid subgrid)
+    {
+        HashSet<Cell> set = new HashSet<Cell>(subgrid.getCells());
+        ifwin = subgrid.getCells().size() == set.size();
+        return ifwin;
+    }
+    
+    public boolean testAll(Grid grid){
+        return true;
+    }
 }
