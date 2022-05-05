dd77534ea9ff26aef5a888540fdf06b4b634e9fc
Carson He <carsonhe@Carsons-Air.usi.ch>
2022-05-05 17:52:10 +0200

    Finish all methods of game class.

--- src/model/Game.java
+++ src/model/Game.java
@@ -27,7 +27,7 @@ public class Game {
     /**
      * Generate an empty grid the fill some cells to generate a determined Sudoku
      */
-    public void initialize(){
+    public void initialize() {
         ArrayList<Cell> Cells = this.grid.getCells();
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
@@ -76,6 +76,7 @@ public class Game {
         Cells.get(79).changeValue(8);
         Cells.get(83).changeValue(1);
         Cells.get(87).changeValue(3);
+        this.printGrid();
     }
 
     /**
@@ -86,7 +87,8 @@ public class Game {
      * @param value      The new value
      */
     public void changeCellValue(int x, int y, int value) {
-        this.grid.getCells().get(y * 9 + x).changeValue(value);
+        this.grid.getCells().get((y - 1) * 9 + (x - 1)).changeValue(value);
+        this.printGrid();
     }
 
     /**
@@ -96,6 +98,7 @@ public class Game {
      */
     public void emptyCell(int x, int y) {
         this.changeCellValue(x, y, 0);
+        this.printGrid();
     }
 
     /**
@@ -104,9 +107,102 @@ public class Game {
     public void checkWin() {
         this.win.testAll(this.grid);
         if (this.win.getIfwin() == true) {
-            // show the win page
+            System.out.println("You win!");
         } else {
-            // show "conditions are not met", continue the game
+            System.out.println("Whoops...Try it again.");
+            this.printGrid();
         }
+    }
+    
+    /**
+     * Print the current grid
+     */
+    public void printGrid() {
+        ArrayList<Cell> Cells = this.grid.getCells();
+        System.out.println("*************");
+        System.out.println("*" + Integer.toString(Cells.get(0).getValue())
+                               + Integer.toString(Cells.get(1).getValue())
+                               + Integer.toString(Cells.get(2).getValue()) +
+                           "*" + Integer.toString(Cells.get(3).getValue())
+                               + Integer.toString(Cells.get(4).getValue())
+                               + Integer.toString(Cells.get(5).getValue()) +
+                           "*" + Integer.toString(Cells.get(6).getValue())
+                               + Integer.toString(Cells.get(7).getValue())
+                               + Integer.toString(Cells.get(8).getValue()) + "*");
+        System.out.println("*" + Integer.toString(Cells.get(9).getValue())
+                               + Integer.toString(Cells.get(10).getValue())
+                               + Integer.toString(Cells.get(11).getValue()) +
+                           "*" + Integer.toString(Cells.get(12).getValue())
+                               + Integer.toString(Cells.get(13).getValue())
+                               + Integer.toString(Cells.get(14).getValue()) +
+                           "*" + Integer.toString(Cells.get(15).getValue())
+                               + Integer.toString(Cells.get(16).getValue())
+                               + Integer.toString(Cells.get(17).getValue()) + "*");
+        System.out.println("*" + Integer.toString(Cells.get(18).getValue())
+                               + Integer.toString(Cells.get(19).getValue())
+                               + Integer.toString(Cells.get(20).getValue()) +
+                           "*" + Integer.toString(Cells.get(21).getValue())
+                               + Integer.toString(Cells.get(22).getValue())
+                               + Integer.toString(Cells.get(23).getValue()) +
+                           "*" + Integer.toString(Cells.get(24).getValue())
+                               + Integer.toString(Cells.get(25).getValue())
+                               + Integer.toString(Cells.get(26).getValue()) + "*");
+        System.out.println("*************");
+        System.out.println("*" + Integer.toString(Cells.get(27).getValue())
+                               + Integer.toString(Cells.get(28).getValue())
+                               + Integer.toString(Cells.get(29).getValue()) +
+                           "*" + Integer.toString(Cells.get(30).getValue())
+                               + Integer.toString(Cells.get(31).getValue())
+                               + Integer.toString(Cells.get(32).getValue()) +
+                           "*" + Integer.toString(Cells.get(33).getValue())
+                               + Integer.toString(Cells.get(34).getValue())
+                               + Integer.toString(Cells.get(35).getValue()) + "*");
+        System.out.println("*" + Integer.toString(Cells.get(36).getValue())
+                               + Integer.toString(Cells.get(37).getValue())
+                               + Integer.toString(Cells.get(38).getValue()) +
+                           "*" + Integer.toString(Cells.get(39).getValue())
+                               + Integer.toString(Cells.get(40).getValue())
+                               + Integer.toString(Cells.get(41).getValue()) +
+                           "*" + Integer.toString(Cells.get(42).getValue())
+                               + Integer.toString(Cells.get(43).getValue())
+                               + Integer.toString(Cells.get(44).getValue()) + "*");
+        System.out.println("*" + Integer.toString(Cells.get(45).getValue())
+                               + Integer.toString(Cells.get(46).getValue())
+                               + Integer.toString(Cells.get(47).getValue()) +
+                           "*" + Integer.toString(Cells.get(48).getValue())
+                               + Integer.toString(Cells.get(49).getValue())
+                               + Integer.toString(Cells.get(50).getValue()) +
+                           "*" + Integer.toString(Cells.get(51).getValue())
+                               + Integer.toString(Cells.get(52).getValue())
+                               + Integer.toString(Cells.get(53).getValue()) + "*");
+        System.out.println("*************");
+        System.out.println("*" + Integer.toString(Cells.get(54).getValue())
+                               + Integer.toString(Cells.get(55).getValue())
+                               + Integer.toString(Cells.get(56).getValue()) +
+                           "*" + Integer.toString(Cells.get(57).getValue())
+                               + Integer.toString(Cells.get(58).getValue())
+                               + Integer.toString(Cells.get(59).getValue()) +
+                           "*" + Integer.toString(Cells.get(60).getValue())
+                               + Integer.toString(Cells.get(61).getValue())
+                               + Integer.toString(Cells.get(62).getValue()) + "*");
+        System.out.println("*" + Integer.toString(Cells.get(63).getValue())
+                               + Integer.toString(Cells.get(64).getValue())
+                               + Integer.toString(Cells.get(65).getValue()) +
+                           "*" + Integer.toString(Cells.get(66).getValue())
+                               + Integer.toString(Cells.get(67).getValue())
+                               + Integer.toString(Cells.get(68).getValue()) +
+                           "*" + Integer.toString(Cells.get(69).getValue())
+                               + Integer.toString(Cells.get(70).getValue())
+                               + Integer.toString(Cells.get(71).getValue()) + "*");
+        System.out.println("*" + Integer.toString(Cells.get(72).getValue())
+                               + Integer.toString(Cells.get(73).getValue())
+                               + Integer.toString(Cells.get(74).getValue()) +
+                           "*" + Integer.toString(Cells.get(75).getValue())
+                               + Integer.toString(Cells.get(76).getValue())
+                               + Integer.toString(Cells.get(77).getValue()) +
+                           "*" + Integer.toString(Cells.get(78).getValue())
+                               + Integer.toString(Cells.get(79).getValue())
+                               + Integer.toString(Cells.get(80).getValue()) + "*");
+        System.out.println("*************");
     }
 }
