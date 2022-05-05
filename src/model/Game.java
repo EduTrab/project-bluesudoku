7d3284a16ec32b757d371e3d3c885016f6830c9c
Carson He <carsonhe@Carsons-Air.usi.ch>
2022-05-05 16:48:57 +0200

    Create a method of Game class to initialize the Sudoku

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  * The main class of the sudoku game, which the player directly interact with.
  *
@@ -9,28 +11,82 @@ package model;
 public class Game {
     // instance variables - replace the example below with your own
     private Grid grid;
-    private Win win;
+    private AllWin win;
 
     /**
      * Constructor for objects of class Game
      */
     public Game() {
         this.grid = new Grid();
-        this.win = new Win();
+        this.win = new AllWin();
     }
 
+    // TODO: An algorithm to randomly generate the values
+    // Now we just give a determined Sudoku
+    
     /**
-     * TODO: An algorithm to randomly generate the values
+     * Generate an empty grid the fill some cells to generate a determined Sudoku
      */
+    public void initialize(){
+        ArrayList<Cell> Cells = this.grid.getCells();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                int current = i * 9 + j;
+                Cells.add(current, new Cell(j+1, i+1));
+            }
+        }
+        Cells.get(1).changeValue(3);
+        Cells.get(2).changeValue(2);
+        Cells.get(3).changeValue(1);
+        Cells.get(4).changeValue(5);
+        Cells.get(11).changeValue(5);
+        Cells.get(15).changeValue(2);
+        Cells.get(17).changeValue(8);
+        Cells.get(18).changeValue(1);
+        Cells.get(19).changeValue(9);
+        Cells.get(21).changeValue(2);
+        Cells.get(23).changeValue(4);
+        Cells.get(24).changeValue(7);
+        Cells.get(26).changeValue(3);
+        Cells.get(27).changeValue(2);
+        Cells.get(28).changeValue(5);
+        Cells.get(29).changeValue(7);
+        Cells.get(31).changeValue(3);
+        Cells.get(33).changeValue(6);
+        Cells.get(34).changeValue(4);
+        Cells.get(36).changeValue(9);
+        Cells.get(37).changeValue(8);
+        Cells.get(40).changeValue(7);
+        Cells.get(44).changeValue(1);
+        Cells.get(45).changeValue(6);
+        Cells.get(50).changeValue(9);
+        Cells.get(51).changeValue(8);
+        Cells.get(52).changeValue(2);
+        Cells.get(53).changeValue(7);
+        Cells.get(54).changeValue(3);
+        Cells.get(55).changeValue(7);
+        Cells.get(56).changeValue(8);
+        Cells.get(60).changeValue(9);
+        Cells.get(63).changeValue(4);
+        Cells.get(64).changeValue(7);
+        Cells.get(65).changeValue(8);
+        Cells.get(69).changeValue(9);
+        Cells.get(72).changeValue(4);
+        Cells.get(76).changeValue(6);
+        Cells.get(79).changeValue(8);
+        Cells.get(83).changeValue(1);
+        Cells.get(87).changeValue(3);
+    }
 
     /**
      * Change the value of a cell
      * 
-     * @param cellNumber The object cell
+     * @param x The x coordinate of the object cell
+     * @param y The y coordinate of the object cell
      * @param value      The new value
      */
-    public void changeCellValue(int cellNumber, int value) {
-        this.grid.getCells().get(cellNumber).changeValue(value);
+    public void changeCellValue(int x, int y, int value) {
+        this.grid.getCells().get(y * 9 + x).changeValue(value);
     }
 
     /**
@@ -38,8 +94,8 @@ public class Game {
      * 
      * @param cellNumber The object cell
      */
-    public void emptyCell(int cellNumber) {
-        this.changeCellValue(cellNumber, 0);
+    public void emptyCell(int x, int y) {
+        this.changeCellValue(x, y, 0);
     }
 
     /**
