d8ef694323ed74d8387c8b4c1868eb465eed00ac
Eduardo Trabattoni <trabae@usi.ch>
2022-05-29 13:42:42 +0200

    gui connection to game

--- src/gui/GridLayoutManeger.java
+++ src/gui/GridLayoutManeger.java
@@ -21,22 +21,23 @@ public class GridLayoutManeger {
 
     /**
      * TODO.
-     * @return MainCenter 
+     * 
+     * @return MainCenter
      */
     public JPanel createGrid() {
         // TODO add param to createGride once it is automated(greats grids input)
         JPanel mainCenter = new JPanel(new GridLayout(9, 9));
 
         int[][] sudoku = {
-                { 7, 0, 2, 0, 5, 0, 6, 0, 0 },
-                { 0, 0, 0, 0, 0, 3, 0, 0, 0 },
-                { 1, 0, 0, 0, 0, 9, 5, 0, 0 },
-                { 8, 0, 0, 0, 0, 0, 0, 9, 0 },
-                { 0, 4, 3, 0, 0, 0, 7, 5, 0 },
-                { 0, 9, 0, 0, 0, 0, 0, 0, 8 },
-                { 0, 0, 9, 7, 0, 0, 0, 0, 5 },
-                { 0, 0, 0, 2, 0, 0, 0, 0, 0 },
-                { 0, 0, 7, 0, 4, 0, 2, 0, 3 }
+                { 0, 9, 7, 3, 8, 1, 2, 4, 5 },
+                { 8, 1, 3, 4, 2, 5, 7, 6, 9 },
+                { 2, 4, 5, 6, 7, 9, 1, 8, 3 },
+                { 9, 3, 4, 5, 1, 8, 6, 7, 2 },
+                { 7, 6, 8, 9, 3, 2, 4, 5, 1 },
+                { 5, 2, 1, 7, 4, 6, 9, 3, 8 },
+                { 1, 7, 9, 8, 6, 3, 5, 2, 4 },
+                { 3, 5, 6, 2, 9, 4, 8, 1, 7 },
+                { 4, 8, 2, 1, 5, 7, 3, 9, 6 }
         }; // TODO automat sudoku insertion
 
         // initialys JLabel
